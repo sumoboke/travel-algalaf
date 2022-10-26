d6109be813b2337be7917ef5df6cfd4d9c72023e
web6dev <calista@web6devs-MacBook-Air.local>
2022-10-27 00:25:52 +0700

    rep

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = () => {
       ) : (
         <div className="w-screen h-screen flex bg-gradient-to-br from-orange-700/50 via-amber-300/50">
           <div className="m-auto flex flex-col py-3 px-2 bg-white/20 backdrop-blur-md backdrop-contrast-75">
-            SignUp
+            <form></form>
           </div>
         </div>
       )}
